c9998683c0f921abb1f32be01af646535c7cec18
willson lee <bingzhen001@qq.com>
2018-04-28 18:55:42 +0800

    1、优化绑定设备

---
+++ app/src/main/java/me/aidoc/client/custom/StringPickerDialog.java
@@ -0,0 +1,145 @@
+package me.aidoc.client.custom;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.miao.demo.R;
+
+
+public class StringPickerDialog extends Dialog {
+
+    private Params params;
+    public StringPickerDialog(Context context) {
+        super(context,R.style.mycenter_ms_dialog);
+    }
+
+    public StringPickerDialog(Context context, int themeResId) {
+        super(context, themeResId);
+    }
+
+    private void setParams(StringPickerDialog.Params params) {
+        this.params = params;
+    }
+
+
+    public void setSelection(String itemValue) {
+        if (params.dataList.size() > 0) {
+            int idx = params.dataList.indexOf(itemValue);
+            if (idx >= 0) {
+                params.initSelection = idx;
+                params.loopData.setCurrentItem(params.initSelection);
+            }
+        }
+    }
+
+    public interface OnDataSelectedListener {
+        void onDataSelected(String itemValue);
+    }
+
+    private static final class Params {
+        private boolean shadow = true;
+        private boolean canCancel = true;
+        private LoopView loopData;
+        private String title;
+        private String unit;
+        private int initSelection,initSelection2;
+        private OnDataSelectedListener callback;
+        private final List<String> dataList = new ArrayList<>();
+    }
+
+    public static class Builder {
+        private final Context context;
+        private final StringPickerDialog.Params params;
+
+        public Builder(Context context) {
+            this.context = context;
+            params = new StringPickerDialog.Params();
+        }
+
+        private final String getCurrDateValue() {
+            return params.loopData.getCurrentItemValue();
+        }
+        public Builder setData(List<String> dataList) {
+            params.dataList.clear();
+            params.dataList.addAll(dataList);
+            return this;
+        }
+        public Builder setTitle(String title) {
+            params.title = title;
+            return this;
+        }
+
+        public Builder setUnit(String unit) {
+            params.unit = unit;
+            return this;
+        }
+
+        public Builder setSelection(int selection) {
+            params.initSelection = selection;
+            return this;
+        }
+        public Builder setOnDataSelectedListener(OnDataSelectedListener onDataSelectedListener) {
+            params.callback = onDataSelectedListener;
+            return this;
+        }
+
+
+        public StringPickerDialog create() {
+            final StringPickerDialog dialog = new StringPickerDialog(context);
+            View view = LayoutInflater.from(context).inflate(R.layout.dc_string_picker, null);
+
+            if (!TextUtils.isEmpty(params.title)) {
+                TextView tvTitle = (TextView) view.findViewById(R.id.tv_title);
+                tvTitle.setText(params.title);
+            }
+
+            final LoopView loopData = (LoopView) view.findViewById(R.id.loop_data);
+            loopData.setArrayList(params.dataList);
+            loopData.setNotLoop();
+            loopData.setRect();
+            if (params.dataList.size() > 0) loopData.setCurrentItem(params.initSelection);
+            view.findViewById(R.id.tv_finish).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                    params.callback.onDataSelected(getCurrDateValue());
+                }
+            });
+            view.findViewById(R.id.tv_cancel).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+            Window win = dialog.getWindow();
+            View dialogView = win.getDecorView();
+            dialogView.setBackgroundResource(R.color.transparent);
+            WindowManager.LayoutParams lp = win.getAttributes();
+            lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+            lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+            win.setAttributes(lp);
+            win.setGravity(Gravity.BOTTOM);
+//            win.setWindowAnimations(R.style.task_animation);
+
+            dialog.setContentView(view);
+            dialog.setCanceledOnTouchOutside(params.canCancel);
+            dialog.setCancelable(params.canCancel);
+
+            params.loopData = loopData;
+            dialog.setParams(params);
+
+            return dialog;
+        }
+    }
+}
---
+++ app/src/main/java/me/aidoc/client/custom/UnbindDeviceDialog.java
@@ -0,0 +1,59 @@
+package me.aidoc.client.custom;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import cn.miao.demo.R;
+
+
+/**
+ * 解绑弹出框
+ * Created by shule on 2016/3/31.
+ */
+public class UnbindDeviceDialog extends Dialog {
+
+    private Context context;
+    private TextView textView_title_mydialog;
+    private Button button_sure, button_cancel;
+    private View.OnClickListener onClickListener;
+
+    public UnbindDeviceDialog(Context context) {
+        super(context);
+        this.context = context;
+
+    }
+
+    public UnbindDeviceDialog(Context context, View.OnClickListener onClickListener){
+        super(context,R.style.mycenter_ms_dialog);
+        this.context = context;
+        this.onClickListener = onClickListener;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.unbind_device_dialog_layout);
+        initView();
+    }
+
+    private void initView(){
+        button_sure = (Button)findViewById(R.id.button_sure);
+        button_cancel = (Button)findViewById(R.id.button_cancel);
+        textView_title_mydialog = (TextView)findViewById(R.id.textView_title_mydialog);
+        button_sure.setOnClickListener(onClickListener);
+        button_cancel.setOnClickListener(onClickListener);
+    }
+
+    public void setTitleType(int type){
+        if(type == 1){
+            textView_title_mydialog.setText("是否解绑所有设备？");
+        }else if(type == 2){
+            textView_title_mydialog.setText("是否解绑该设备？");
+        }
+    }
+}
