8d5f54f3493459ecb315672ddcf7adac1ae97efc
willson lee <bingzhen001@qq.com>
2018-06-06 10:13:11 +0800

    1、wechat pay fix

---
+++ app/src/main/java/me/aidoc/client/cslib/Constants.java
@@ -0,0 +1,12 @@
+package me.aidoc.client.cslib;
+
+public class Constants {
+	// APP_ID �滻Ϊ���Ӧ�ôӹٷ���վ���뵽�ĺϷ�appId
+    public static final String APP_ID = "wxd930ea5d5a258f4f";
+
+    public static class ShowMsgActivity {
+		public static final String STitle = "showmsg_title";
+		public static final String SMessage = "showmsg_message";
+		public static final String BAThumbData = "showmsg_thumb_data";
+	}
+}
