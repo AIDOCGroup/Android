efebfd4ef8ce9efb1f35152c874acc84ab0eea93
willson lee <bingzhen001@qq.com>
2018-07-11 19:57:23 +0800

    1、bugfix 2、add BleConnect

--- app/src/main/java/me/aidoc/client/custom/BezierLayout.java
+++ app/src/main/java/me/aidoc/client/custom/BezierLayout.java
@@ -1,11 +1,16 @@
 package me.aidoc.client.custom;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
@@ -119,5 +124,48 @@ public class BezierLayout extends LinearLayout {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpValue, getResources().getDisplayMetrics());
     }
 
+    /**
+     * 设置新的绘图模式---更新视图
+     *
+     * @param mode
+     */
+    public void setPorterDuffMode(PorterDuff.Mode mode) {
+        this.mXfermode = new PorterDuffXfermode(mode);
+        mBitmap = null;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (mBitmap == null) {
+            createBitmap();
+        }
+        canvas.drawBitmap(mBitmap, 0, 0, mPaint);
+    }
+
+    // 绘制结束之后mBitmap上就有了像素信息
+    private void createBitmap() {
+        final int width = getWidth();
+        final int height = getHeight();
+        final int minDimension = Math.min(width, height);
+        final Rect rect = new Rect();
+        rect.right = minDimension - 1;
+        rect.bottom = rect.right;
+        mBitmap = Bitmap.createBitmap(minDimension, minDimension,
+                Bitmap.Config.ARGB_8888);
+
+        Bitmap src = BitmapFactory.decodeResource(getResources(),
+                R.drawable.skia_dst);
+        final Canvas canvas = new Canvas(mBitmap);
+        canvas.drawBitmap(src, null, rect, mPaint);// 绘制到目标矩形上
+
+        src = BitmapFactory.decodeResource(getResources(), R.drawable.skia_src);
+
+        mPaint.setXfermode(mXfermode);
+        canvas.drawBitmap(src, null, rect, mPaint);
+
+        mPaint.setXfermode(null);
+
+    }
 
 }
